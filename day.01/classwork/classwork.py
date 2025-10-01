a3be4da469851a7bc5f816bce8b5ca16b4d5c13a
nanuka614 <dekanosidze.nanuka@icloud.com>
2025-10-01 22:38:45 +0400

    GOA

---
+++ day.01/classwork/classwork.py
@@ -0,0 +1,15 @@
+name="nana"
+print(name)
+
+name1="irakli"
+name2="ana"
+name3="mari"
+
+print(name1)
+print(name2)
+print(name3)
+
+name="nanuka"
+age=15
+
+print(name, age)
---
+++ day.01/homework/homework.py
@@ -0,0 +1,27 @@
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+
+
+print("burger")
+print("fries")
+print("watermelon")
+print("wrap")
+print("wings")
+
+
+var="hello"
+name="nanu"
+print(var, name)
+
+
+print("*********")
+print("**HELLO**")
+print("*********")
+
+
+print("burger", 20, "$")
+print("fries", 11, "$")
+print("coke", 3, "$")
