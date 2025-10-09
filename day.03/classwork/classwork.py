31d05e7a7c2944b4f929a7f15242c5998c143b7f
nanuka614 <dekanosidze.nanuka@icloud.com>
2025-10-09 23:31:57 +0400

    GOA

---
+++ day.03/classwork/classwork.py
@@ -0,0 +1,19 @@
+age = input("Enter your age: ")
+print("your age is", age)
+
+color = input("Favorite color: ")
+number = input("Favorite number: ")
+animal = input("Favorite animal: ")
+
+print("You like the color", color)
+print("Your fave number is", number)
+print("You fav animal is", animal)
+
+a = input("enter something: ")
+b = input("enter something: ")
+c = input("enter something: ")
+
+print("List:")
+print("1.", a)
+print("2.", b)
+print("3.", c)
---
+++ day.03/homework/homework.py
@@ -0,0 +1,13 @@
+name=input("enter your name: ")
+age=int(input("enter your age: "))
+print("hello" + name , "your age is:" , age)
+
+food=input("enter your fave food: ")
+print("i love", food)
+
+
+user=input("write something: ")
+
+print("############")
+print("##",user,"##")
+print("############")
