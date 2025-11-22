ae044db36df8f84ac23d32a53d17acb5f560b530
nanuka614 <dekanosidze.nanuka@icloud.com>
2025-11-22 18:30:21 +0400

    GOA BEST

---
+++ day.36/homework/homework.py
@@ -0,0 +1,14 @@
+my_friend = {
+    "name": "ana",
+    "surname": "karenina",
+    "age": 25
+}
+print("My friend:", my_friend)
+
+
+
+
+keys = my_friend.keys()
+print("Keys:", keys)
+values = my_friend.values()
+print("Values:", values)
